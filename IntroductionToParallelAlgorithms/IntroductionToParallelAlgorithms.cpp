c66ed43bd2d1cf49912c60bb014f5f45375ed708
Семен Серов <SYSerov@stud.kpfu.ru>
2023-04-08 15:20:40 +0300

    Добавлена циклическая редукция

--- IntroductionToParallelAlgorithms/IntroductionToParallelAlgorithms.cpp
+++ IntroductionToParallelAlgorithms/IntroductionToParallelAlgorithms.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include "TridiagonalMatrix.h"
 
 std::vector<double> CreateRandomVector(size_t n)
 {
@@ -13,11 +14,12 @@ std::vector<double> CreateRandomVector(size_t n)
     }
     return result;
 }
+
 ///// <summary>
-//        /// Прямой ход метода Прогонки
-//        /// </summary>
-//        /// <param name="matrix"></param>
-//        /// <param name="b"></param>
+///// Прямой ход метода Прогонки
+///// </summary>
+///// <param name="matrix"></param>
+///// <param name="b"></param>
 //static void DirectCourse(TridiagonalMatrix matrix, double[] b)
 //{
 //    double a;
@@ -58,6 +60,38 @@ std::vector<double> CreateRandomVector(size_t n)
 //    string header = "Метод Прогонки";
 //    reporter.Add(header, headers, DataToTable(y));
 //}
+
+std::vector<double> CyclicReduction(std::vector<double> a, std::vector<double> b, std::vector<double> c, std::vector<double> f, const int q, const int n)
+{
+    for (int k = 1; k < q; k++)
+    {
+        int twoInK = pow(2, k);
+        double twoInKminusOne = pow(2, k - 1);
+        for (int i = twoInK; i < n; i += twoInK)
+        {
+            double P = a[i] / b[i - twoInKminusOne];
+            double Q = c[i] / b[i + twoInKminusOne];
+            a[i] = P * a[i - twoInKminusOne];
+            b[i] = b[i] - P * c[i - twoInKminusOne] - Q * a[i + twoInKminusOne];
+            c[i] = Q * c[i + twoInKminusOne];
+            f[i] = f[i] + P * f[i - twoInKminusOne] + Q * f[i + twoInKminusOne];
+        }
+    }
+    std::vector<double> x(n + 1);
+    x[0] = f[0];
+    x[n] = f[n];
+    for (int k = q; k > 0; k--)
+    {
+        int twoInK = pow(2, k);
+        double twoInKminusOne = pow(2, k - 1);
+        for (int i = twoInKminusOne; i <= n - twoInKminusOne; i += twoInK)
+        {
+            x[i] = (f[i] + a[i] * x[i - twoInKminusOne] + c[i] * x[i + twoInKminusOne]) / b[i];
+        }
+    }
+    return x;
+}
+
 int main()
 {
     int q = 4; //n=2^q - количество уравнений
@@ -75,11 +109,17 @@ int main()
     std::vector<double> f(n + 1);
     f[0] = x[0];
     f[n] = x[n];
-    std::cout << x[0] << '=' << f[0] << std::endl;
+    //std::cout << x[0] << '=' << f[0] << std::endl;
     for (int i = 1; i < n; i++)
     {
         f[i] = -a[i] * x[i - 1] + b[i] * x[i] - c[i]*x[i + 1];
-        std::cout << -a[i] << '*' << x[i - 1] << '+' << b[i] << '*' << x[i] << '-' << c[i] << '*' << x[i + 1] << '=' << f[i] << std::endl;
+        //std::cout << -a[i] << '*' << x[i - 1] << '+' << b[i] << '*' << x[i] << '-' << c[i] << '*' << x[i + 1] << '=' << f[i] << std::endl;
     }
-    std::cout << x[n] << '=' << f[n] << std::endl;
+    //std::cout << x[n] << '=' << f[n] << std::endl;
+
+    std::vector<double> res = CyclicReduction(a, b, c, f, q, n);
+    for (int i = 0; i < res.size(); i++)
+    {
+        std::cout << x[i] << " " << res[i] << std::endl;
+    }
 }
